8bc03d489807a734811b0e83fbadcc832b9a4239
LuigiVessella <vessellaluigi12@gmail.com>
2017-12-03 18:56:52 +0100

    file nuovo

---
+++ lista.py
@@ -0,0 +1,8 @@
+una_lista = [1, 2, 3, 'hello', 4.2, (0, 0)]
+print "Ho memorizzato la lista seguente: "
+print una_lista
+
+print "Gli elementi uno per riga sono: "
+
+for elemento in una_lista:
+    print elemento
